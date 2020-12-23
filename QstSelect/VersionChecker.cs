beb695b1fe5097b8908a9c7c02c52cdb330aea5c
AyrA <github@bitm.sg>
2020-12-23 11:07:03 +0100

    Support save games

--- QstSelect/VersionChecker.cs
+++ QstSelect/VersionChecker.cs
@@ -25,36 +25,67 @@ namespace QstSelect
 
         public static VersionType GetVersion(string Filename)
         {
-            try
+            string FileContent = null;
+            if (Filename == null)
             {
-                using (var P = new Process())
+                return VersionType.Invalid;
+            }
+            if (Filename.ToLower().EndsWith(".quest-save"))
+            {
+                //save games are not compressed.
+                //Read as text directly
+                try
                 {
-                    P.StartInfo.FileName = Config.Get("7z");
-                    P.StartInfo.Arguments = string.Format(CMDLINE, Filename);
-                    P.StartInfo.UseShellExecute = false;
-                    P.StartInfo.RedirectStandardOutput = true;
-                    if (P.Start())
-                    {
-                        var Output = P.StandardOutput.ReadToEnd();
-                        if (Output.Length > 0)
-                        {
-                            var M = Regex.Match(Output, REGEX);
-                            if(M.Success)
-                            {
-                                var V = int.Parse(M.Groups[1].Value);
-                                return V >= 580 ? VersionType.New : VersionType.Old;
-                            }
-                            return VersionType.Invalid;
-                        }
-                        return File.Exists(Filename) ? VersionType.Invalid : VersionType.NotFound;
-                    }
+                    FileContent = File.ReadAllText(Filename);
+                }
+                catch (FileNotFoundException)
+                {
+                    return VersionType.NotFound;
+                }
+                catch
+                {
                     return VersionType.Error;
                 }
             }
-            catch
+            else
             {
-                return VersionType.Error;
+                //Game files are compressed zip archive.
+                //Extract the game.aslx for version check
+                try
+                {
+                    using (var P = new Process())
+                    {
+                        P.StartInfo.FileName = Config.Get("7z");
+                        P.StartInfo.Arguments = string.Format(CMDLINE, Filename);
+                        P.StartInfo.UseShellExecute = false;
+                        P.StartInfo.RedirectStandardOutput = true;
+                        if (P.Start())
+                        {
+                            FileContent = P.StandardOutput.ReadToEnd();
+                        }
+                        else
+                        {
+                            return VersionType.Error;
+                        }
+                    }
+                }
+                catch
+                {
+                    return VersionType.Error;
+                }
             }
+            //Parse and validate version string
+            if (!string.IsNullOrEmpty(FileContent))
+            {
+                var M = Regex.Match(FileContent, REGEX);
+                if (M.Success)
+                {
+                    var V = int.Parse(M.Groups[1].Value);
+                    return V >= 580 ? VersionType.New : VersionType.Old;
+                }
+                return VersionType.Invalid;
+            }
+            return File.Exists(Filename) ? VersionType.Invalid : VersionType.NotFound;
         }
     }
 }
